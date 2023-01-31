b88a5b92be09b27da4c6b6af4a26965c551a2d3d
Miranda <120993025+Miranda-Bai@users.noreply.github.com>
2023-01-31 17:59:05 +1300

    20230131

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+import "./App.css";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(
+          `https://api.api-ninjas.com/v1/celebrity?name=${searchTerm}`,
+          {
+            headers: {
+              "X-Api-Key": "xSj/4crTRkHqLw1/o3jT3g==4B6VG3HdyNBsrnjH",
+            },
+          }
+        );
+        const data = await res.json();
+        setSearchResult(data);
+      } catch (error) {
+        setError(error);
+      }
+      setIsLoading(false);
+    };
+    if (searchTerm.length > 0) {
+      fetchData();
+    }
+  }, [searchTerm]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>Search for a celebrity networth:</label>
+        <input
+          type="text"
+          id="search"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        ></input>
+      </form>
+      {isLoading ? (
+        <p>One moment please.....</p>
+      ) : (
+        searchResult.map((result) => {
+          return (
+            <div key={nanoid()}>
+              <h2>{result.name}</h2>
+              <h3>Nepo baby net worth: {result.net_worth}</h3>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }
